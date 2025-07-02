d39e3cc19bd9e12fe63cb9ff8505dee40d433a50
Kangminsang <kangminsang1219@gmail.com>
2025-07-02 21:00:36 +0900

    path traversal 방지 로직 추가

--- download.php
+++ download.php
@@ -17,7 +17,19 @@ if (!$file) {
     die('파일이 존재하지 않습니다.');
 }
 
-$filePath = UPLOAD_DIR . $file['file_name'];
+// 첨부파일 경로 검증
+
+// 파일명에서 Path traversal 관련 문자 제거
+$fileName = basename($file['file_name']);
+
+// 실제 파일 경로 계산
+$filePath = UPLOAD_DIR . $fileName;
+
+// 파일이 실제로 UPLOAD_DIR 내에 있는지 검증
+if (strpos(realpath($filePath), realpath(UPLOAD_DIR)) !== 0) {
+    die('잘못된 접근입니다.');
+}
+
 if (!file_exists($filePath)) {
     die('파일을 찾을 수 없습니다.');
 }
@@ -28,4 +40,3 @@ header('Content-Disposition: attachment; filename="' . basename($file['original_
 header('Content-Length: ' . filesize($filePath));
 readfile($filePath);
 exit;
-
